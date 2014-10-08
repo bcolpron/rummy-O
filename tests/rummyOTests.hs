b2bf25ddfe19c1ee456df9a30c6c900db261f42a
Bertin Colpron <bcolpron@innobec.com>
2014-10-07 22:45:45 -0400

    slow moving to HUnit

--- tests/rummyOTests.hs
+++ tests/rummyOTests.hs
@@ -78,9 +78,9 @@ isValidSuite = testEmptyIsNotValid
     && testJokerDoesBreakSeries
     
 
-testCanInsertSameNumberTileToSequenceOfSame = TestCase (assertBool "testCanInsertSameNumberTileToSequenceOfSame" (
-    canInsert [red4, blue4, black4] yellow4
-    && canInsert [blue1, red1] yellow1))
+testCanInsertSameNumberTileToSequenceOfSame = TestList [
+    TestCase (assertBool "canInsert yellow4 in [red4, blue4, black4]" (canInsert [red4, blue4, black4] yellow4)),
+    TestCase (assertBool "canInsert yellow1 in [blue1, red1]"         (canInsert [blue1, red1] yellow1)) ]
 
 testCanInsertConsecutiveTileToSeries = TestCase (assertBool "testCanInsertConsecutiveTileToSeries" (
     canInsert [red1,red2,red3] red4
